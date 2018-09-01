1a5c337b2039b6088d6d85bc5f46e4f4b767aaa4
syeutyu <syeuty@naver.com>
2018-09-01 20:55:17 +0900

    question next error handler

--- src/routes/question/controller.js
+++ src/routes/question/controller.js
@@ -3,15 +3,25 @@ const database = require('../../models').connect();
 exports.question = async (req, res) => {
   const connect = await database;
   const response = {};
-  const query = await connect.query(
-    `SELECT Q.question, A.id, A.is_leaf, A.description, A.next_question_id FROM
-    questions as Q LEFT OUTER JOIN answers as A ON A.question_id = Q.id
-    where Q.id = ${req.query["parent_id"]}`);
 
-  response["question"] = query[0].question;
-  response["answers"] = query.map((item) => {
-    delete item.question;
-    return item;
-  });
-  res.json(response);
+  if(!req.query["parent_id"] || req.query["parent_id"] < 1) {
+    req.query["parent_id"] = 1;
+  }
+
+  try {
+    const query = await connect.query(
+      `SELECT Q.question, A.id, A.is_leaf, A.description, A.next_question_id FROM
+      questions as Q LEFT OUTER JOIN answers as A ON A.question_id = Q.id
+      where Q.id = ${req.query["parent_id"]}`);
+
+    response["question"] = query[0].question;
+    response["answers"] = query.map((item) => {
+      delete item.question;
+      return item;
+    });
+
+    res.json(response);
+    } catch(err) {
+      res.status(500).json(err);
+    }
 }
